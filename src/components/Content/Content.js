52c7006a357e2d03b949fdce206ccf5946b25088
Anthony Taylor <taylortheory@Anthonys-iMac.AN-700-AP-I-AC>
2019-10-17 02:46:50 -0400

    adding moment to project

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,17 +4,27 @@ import Countdown from '../Countdown/Countdown';
 import Voting from '../Voting/Voting';
 import '../../../src/App.css';
 
+import * as moment from 'moment';
 
 const Content = ({ styles }) => {
 
-// const currDay = moment("2018-01-01");
-// const birthday = moment("2018-02-16");
+const now = moment().format("YYYY-MM-DD");
+const conception = moment("2019-07-21")
+// const birthday = moment("2020-04-30");
+const currDay = moment(now);
 
 // const diff = moment.duration(birthday.diff(currDay));
+const togo = moment.duration(currDay.diff(conception));
+
+console.log(now)
 
 // console.log(diff.months() + " months, " + diff.weeks() + " weeks, " + diff.days()%7 + " days.");
 
 // console.log(Math.floor(diff.asWeeks()) + " weeks, " + diff.days()%7 + " days.");
+
+// console.log(togo.months() + " months, " + togo.weeks() + " weeks, " + togo.days()%7 + " days.");
+
+console.log(Math.floor(togo.asWeeks()) + " weeks, " + togo.days()%7 + " days.");
 
   const contentStyle = {
     paddingTop: styles.showSidebar ? 20 : styles.topBarHeight + 20,
@@ -29,6 +39,7 @@ const Content = ({ styles }) => {
           <Voting />
             <div>
             <Countdown date={`2020-04-29T00:00:00`} />
+            <p>{Math.floor(togo.asWeeks()) + " weeks, " + togo.days()%7 + " days."}</p>
     
             </div>
           </div>
